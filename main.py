732087a014a9fd53b62c628560c1a524f87203cf
Rimon Kayastha <rimonkayastha@gmail.com>
2025-06-18 15:58:46 +0545

    Total Expenses, Total Income and Net Balance is now displayed. Also enabled the ability to manage another date.

--- main.py
+++ main.py
@@ -38,6 +38,18 @@ def add_record():
   input_tuple = (input_list)
   ws.append(input_tuple)
   currentfile.save(filepath)
+  
+# Income, Expense, Balance stats display
+def stats():
+  Total_Income = 0
+  for i in range(2, ws.max_row+1):
+    Total_Income = Total_Income + int(ws.cell(row=i, column = 4).value)
+  Total_Expense = 0
+  for i in range(2, ws.max_row+1):
+    Total_Expense = Total_Expense + int(ws.cell(row=i, column = 5).value)
+  Net_Balance = int(ws.cell(row=ws.max_row, column = ws.max_column).value)
+
+  print(f'As of now, the total income is {Total_Income}, the total expense is {Total_Expense} and the net balance is {Net_Balance}')
 
 #Title
 Titlestr = 'DAILY EXPENSE TRACKER'
@@ -45,38 +57,46 @@ trgtdate = date.today()
 print(f"\n{Titlestr:-^70} \nToday's Date: {trgtdate}")
 
 #Optional Date Input
-diffdate = input("Would you like to track expenses for a different date (Y/N)? ")
-if diffdate.upper() == 'Y':
+diffdate = input("Would you like to manage or track income/expenses for a different date (Y/N)? ")
+while diffdate.upper() == 'Y':
   trgtdate = input('Enter target date in YYYY-MM-DD format (Include \'-\'): ')
 
-# File Existence Check
-filename = f'{trgtdate}-expenses.xlsx'
-foldername = 'Daily-Income-Expense-Sheets'
-filepath = os.path.join(foldername, filename)
-file_exist = False
-if os.path.exists(filepath) and os.path.isfile(filepath):
-  currentfile = file_append(filepath) # Append file since file exists
-else:
-  currentfile = file_creation(filepath) # Create file since file does not exist
-
-# Default Header Writing
-ws = currentfile.active
-headertitles = ['Time', 'Description', 'Category', 'Income', 'Expense', 'Balance']
-if file_exist is False:
-  for i in range(6):
-    ws.cell(row=1, column = i+1, value = headertitles[i])
-  currentfile.save(filepath)
-  
-# Record
-print(ws.max_row)
-recordask = input('Would you like to enter a new record (Y/N)? ')
-while recordask.upper() == 'Y':
-  add_record()
-  if ws.max_row==2:
-    ws.cell(row=ws.max_row, column=6, value=(int(ws.cell(row=ws.max_row, column=4).value) - int(ws.cell(row=ws.max_row, column=5).value)))
+  # File Existence Check
+  filename = f'{trgtdate}-expenses.xlsx'
+  foldername = 'Daily-Income-Expense-Sheets'
+  filepath = os.path.join(foldername, filename)
+  file_exist = False
+  if os.path.exists(filepath) and os.path.isfile(filepath):
+    currentfile = file_append(filepath) # Append file since file exists
   else:
-    ws.cell(row=ws.max_row, column=6, value=(int(ws.cell(row=ws.max_row, column=4).value) - int(ws.cell(row=ws.max_row, column=5).value) + int(ws.cell(row=ws.max_row - 1, column=6).value)))
-  currentfile.save(filepath)
+    currentfile = file_creation(filepath) # Create file since file does not exist
+
+  # Default Header Writing
+  ws = currentfile.active
+  headertitles = ['Time', 'Description', 'Category', 'Income', 'Expense', 'Balance']
+  if file_exist is False:
+    for i in range(6):
+      ws.cell(row=1, column = i+1, value = headertitles[i])
+    currentfile.save(filepath)
+    
+  # Record
+  print(ws.max_row)
   recordask = input('Would you like to enter a new record (Y/N)? ')
+  while recordask.upper() == 'Y':
+    add_record()
+    if ws.max_row==2:
+      ws.cell(row=ws.max_row, column=6, value=(int(ws.cell(row=ws.max_row, column=4).value) - int(ws.cell(row=ws.max_row, column=5).value)))
+    else:
+      ws.cell(row=ws.max_row, column=6, value=(int(ws.cell(row=ws.max_row, column=4).value) - int(ws.cell(row=ws.max_row, column=5).value) + int(ws.cell(row=ws.max_row - 1, column=6).value)))
+    currentfile.save(filepath)
+    recordask = input('Would you like to enter a new record (Y/N)? ')
 
+  # Displaying total income, total expense, Net Balance
+  statsask = input('Would you like to view the total income, total expenses and net balance for this day (Y/N)? ')
+  if statsask.upper() == 'Y':
+    stats()
 
+  diffdate = input("Would you like to manage or track income/expenses for a different date (Y/N)? ")
+
+#Ending Message
+print('The application has closed.')
