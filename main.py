3a35f6555f7e592bc2a444b2b47605ea29b04448
Rimon Kayastha <rimonkayastha@gmail.com>
2025-06-18 13:40:27 +0545

    Corrupted Excel File Bug resolved. Using Openpyxl library.

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 #Libraries
 from datetime import date
 import os
+import openpyxl
+from openpyxl import Workbook
+import openpyxl.workbook
 
 # --Functions--
 # Excel File Creation
 def file_creation(folder, file):
-  crntfile = open(os.path.join(folder, file), 'x')
+  crntfile = Workbook()
+  crntfile.save(os.path.join(folder, file))
   print('A new excel sheet has been created.')
   return crntfile
 
 # Excel File Appending
 def file_append(folder, file):
-  filecreate = input('Would you like to overwrite (O) or add to the existing file (A)? ')
-  if filecreate.upper() == 'O':
-    crntfile = open(os.path.join(folder, file), 'w')
-    print('The existing excel sheet will now be overwritten.')
-  elif filecreate.upper() == 'A':
-    crntfile = open(os.path.join(folder, file), 'a')
-    print('The existing excel sheet will now be modified.')
+  crntfile = openpyxl.workbook.load(os.path.join(folder, file))
+  print('The existing excel sheet will now be modified.')
   return crntfile
-    
 
 #Title
 Titlestr = 'DAILY EXPENSE TRACKER'
@@ -36,6 +34,8 @@ filename = f'{trgtdate}-expenses.xlsx'
 foldername = 'Daily-Income-Expense-Sheets'
 filepath = f'./{foldername}/{filename}'
 if os.path.exists(filepath) and os.path.isfile(filepath):
-  currentfile = file_append(foldername, filename)
+  currentfile = file_append(foldername, filename) # Append file since file exists
 else:
-  currentfile = file_creation(foldername, filename)
+  currentfile = file_creation(foldername, filename) # Create file since file does not exist
+
+# Records
