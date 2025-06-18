39ed430f941924c6d7af341bb3c0116807508f9e
Rimon Kayastha <rimonkayastha@gmail.com>
2025-06-18 11:35:02 +0545

    Excel File Naming and File Existence Check

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 #Libraries
 from datetime import date
+import os
 
 #Title
 Titlestr = 'DAILY EXPENSE TRACKER'
-Datetdystr = date.today()
-print(f"\n{Titlestr:-^70} \nToday's Date: {Datetdystr}")
+trgtdate = date.today()
+print(f"\n{Titlestr:-^70} \nToday's Date: {trgtdate}")
 
-#Ask for date
-diffdate = input("Would you like to track expenses of a different date (Y/N)? ")
+#Optional Date Input
+diffdate = input("Would you like to track expenses for a different date (Y/N)? ")
 if diffdate.upper() == 'Y':
   trgtdate = input('Enter target date in YYYY-MM-DD format (Include \'-\'): ')
+
+# File Existence Check
+filename = f'{trgtdate}-expenses.xlsx'
+filepath = f'./Daily-Income-Expense-Sheets/{filename}'
+if os.path.exists(filepath) and os.path.isfile(filepath):
+  print('Already Exists!')
+else:
+  print('Does not Exist')
 
 #Functions
