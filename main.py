dca4a9f59f71e6c0e4b484751a68934c0f6eba71
Rimon Kayastha <rimonkayastha@gmail.com>
2025-06-18 15:30:23 +0545

    Adding Records now possible. However, could not resolve a bug associated to adding a net balance.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Libraries
 from datetime import date
+from datetime import time
+from datetime import datetime
 import os
 import openpyxl
 from openpyxl import Workbook
@@ -15,10 +17,27 @@ def file_creation(path):
 
 # Excel File Appending
 def file_append(path):
-  crntfile = openpyxl.workbook.load(path)
+  crntfile = openpyxl.load_workbook(path)
   print('The existing excel sheet will now be modified.')
   file_exist = True
   return crntfile
+
+# Add a record
+def add_record():
+  input_list = []
+  time_input_dec = input('Would you like to enter time of income/expense (T) or use Current Time (C): ')
+  if time_input_dec.upper() == 'T': #Inputting time
+    time_input = input('Enter the time of income/expense (HH:MM): ')
+  elif time_input_dec.upper() == 'C':
+    current_datetime = datetime.now()
+    time_input = current_datetime.strftime("%H:%M")
+  input_list.append(time_input)
+  for i in range(len(headertitles)-1):
+    user_input = input(f'Enter the {headertitles[i+1]} if applicable: ')
+    input_list.append(user_input)
+  input_tuple = (input_list)
+  ws.append(input_tuple)
+  currentfile.save(filepath)
 
 #Title
 Titlestr = 'DAILY EXPENSE TRACKER'
@@ -47,3 +66,13 @@ if file_exist is False:
   for i in range(5):
     ws.cell(row=1, column = i+1, value = headertitles[i])
   currentfile.save(filepath)
+  
+# Record
+print(ws.max_row)
+recordask = input('Would you like to enter a new record (Y/N)? ')
+while recordask.upper() == 'Y':
+  add_record()
+  # ws.cell(row=ws.max_row, column=ws.max_column, value=((int(ws.cell(row=ws.max_row, column=4).value) - int(ws.cell(row=ws.max_row, column=5).value)) + int(ws.cell(row=ws.max_row - 1, column=ws.max_column).value))) #Code not working for some reason
+  recordask = input('Would you like to enter a new record (Y/N)? ')
+
+
